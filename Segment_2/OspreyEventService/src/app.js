6ef59e16e77aef1056b3b24940765fe811afffad
gimballock <gimballock@gmail.com>
2014-05-26 13:52:16 -0700

    Updated event examples, externalized json files

--- Segment_2/OspreyEventService/src/app.js
+++ Segment_2/OspreyEventService/src/app.js
@@ -3,45 +3,13 @@ var path = require('path');
 var osprey = require('osprey');
 var _ = require('underscore');
 
-var events = [ 
-    { 
-      "id":0,
-      "name":"APICon SF", 
-      "description":"APIcon is a 3-day interactive conference for app developers, existing public API providers, and those currently developing an API strategy.\nFrom May 27-29, 450+ stakeholders in the API economy will gather at the Hilton San Francisco Union Square to learn, create, ideate, and debate while advancing the overall interests of the API economy and all of its constituents. - See more at: http://www.apiconsf.com/#sthash.prwopBLX.dpuf", 
-      "location":"750 Kearny St, San Francisco, CA 94108", 
-      "starts_at":"2014-05-27",
-      "ends_at":"2014-05-29"
-    },
-    { 
-      "id":1,
-      "name":"APICon SF 2015", 
-      "description":"APIcon is a 5-day interactive conference for app developers, existing public API providers, and those currently developing an API strategy.\nFrom May 27-29, 1000+ stakeholders in the API economy will gather at the Hilton San Francisco Union Square to learn, create, ideate, and debate while advancing the overall interests of the API economy and all of its constituents. - See more at: http://www.apiconsf2015.com/not-a-real-url", 
-      "location":"750 Kearny St, San Francisco, CA 94108", 
-      "starts_at":"2015-05-27",
-      "ends_at":"2015-05-29"
-    }
-];
-
-var event_schema = {
-    "$schema": "http://json-schema.org/draft-04/schema",
-    "type" : "object",
-    "properties" : {
-        "id" : { "type" : "number" },
-        "name" : { "type" : "string", "minLength":1 },
-        "description" : { "type" : "string" },
-        "location" : { "type" : "string" },
-        "starts_at" : { "type" : "string" },
-        "ends_at" : { "type" : "string" },
-    }
-};
-
 var app = module.exports = express();
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 };
 
 app.use(express.bodyParser());
@@ -69,46 +37,64 @@ if (!module.parent) {
   console.log('listening on port ' + port);
 }
 
+// -----------------------------------
+// Implementation logic
+// -----------------------------------
+
+//Load JSON resources
+//Assumes .json extension and located within assets dir
+var load_json_file = function (file_name) {
+  try {
+    return require( path.join(__dirname, '/assets/json/'+file_name+'.json') );
+  } catch(err) {
+    console.log('JSON file load error: ' + err);
+    return {};
+  }
+}
+
+var events = load_json_file('events');
+var event_schema = load_json_file('event_schema');
+
+
 var get_handler = function(req, res) {
-    var return_code = 404;
-    var return_value = 'Event could not be found';
+  var return_code = 404;
+  var return_value = 'Event could not be found';
 
-    if (!_.isUndefined(req.params.eid)) {
-        var id = parseInt(req.params.eid, 10);
-        var event = _.find(events, function (event) {
-            if(event.id === id)
-                return true;
-            return false;
-        });
-        if (!_.isUndefined(event)) {
-            return_value = event;
-            return_code = 200;
-        }
-    } else {
-        if (_.has(req.query, 'sort_order')) { 
-            events = _.sortBy(events, function(e) { return e.name; });
-            if(req.query.sort_order === 'desc')
-                events = events.reverse();
-        }
-
-        return_value = events;
-        return_code = 200;
+  if (!_.isUndefined(req.params.eid)) {
+    var id = parseInt(req.params.eid, 10);
+    var event = _.find(events, function (event) {
+      if(event.id === id)
+        return true;
+      return false;
+    });
+    if (!_.isUndefined(event)) {
+      return_value = event;
+      return_code = 200;
     }
-    res.send(return_code, return_value);
+  } else {
+    if (_.has(req.query, 'sort_order')) { 
+      events = _.sortBy(events, function(e) { return e.name; });
+      if(req.query.sort_order === 'desc')
+        events = events.reverse();
+    }
+
+    return_value = events;
+    return_code = 200;
+  }
+  res.send(return_code, return_value);
 };
 
 var post_handler = function (req, res) {
-    var requested_event = req.body;
-    var return_code, return_value;
+  var requested_event = req.body;
+  var return_code, return_value;
 
-    events.push(requested_event);
-    return_code = 201;
-    return_value = "New event created with id=" + requested_event.id;
-    res.send(return_code, { message: return_value } );
+  events.push(requested_event);
+  return_code = 201;
+  return_value = "New event created with id=" + requested_event.id;
+  res.send(return_code, { message: return_value } );
 };
 
 //Adding business logic to a valid RAML Resource
 api.get('/events', get_handler);
-api.get('/events/:eid', get_handler);
-
 api.post('/events', post_handler);
+api.get('/events/:eid', get_handler);
